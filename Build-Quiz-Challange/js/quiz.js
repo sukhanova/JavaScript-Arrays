f1fb67063e4404b464487961db57904c12780ebb
sukhanova <verusia.s@gmail.com>
2020-06-24 22:02:34 -0500

    added multidimensioanl array to hold quiz questions

---
+++ Build-Quiz-Challange/js/quiz.js
@@ -0,0 +1,25 @@
+// 1. Create a multidimensional array to hold quiz questions and answers
+const quizQuestions = [
+  ['How many legs does a spider have?'], ['Eight'],
+  ['What is the name of the toy cowboy in Toy Story?'], ['Woody'],
+  ['Whose nose grew longer every time he lied?'], ['Pinocchio'],
+  ['Which Disney movie is Elsa in?'],['Frozen'],
+  ['What do caterpillars turn into?'], ['Butterflies'],
+  ['What is the color of a school bus?'], ['Yellow'],
+  ['What do you use to write on a blackboard?'], ['Chalk'],
+  ['How many days are in a year?'], [365]
+];
+
+// 2. Store the number of questions answered correctly
+
+
+/* 
+  3. Use a loop to cycle through each question
+      - Present each question to the user
+      - Compare the user's response to answer in the array
+      - If the response matches the answer, the number of correctly
+        answered questions increments by 1
+*/
+
+
+// 4. Display the number of correct answers to the user
